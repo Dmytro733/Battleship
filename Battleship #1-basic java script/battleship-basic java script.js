8fe57274424d76b42c84337bca91a780c6c96a8b
Dmytro733 <ddp0496@gmail.com>
2020-05-26 17:07:15 +0300

    createing new project battleship #1

---
+++ Battleship #1-basic java script/battleship-basic java script.js
@@ -0,0 +1,29 @@
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+let shot;
+let hits = 0;
+let shots = 0;
+
+let isSunk = false;
+
+while (isSunk == false) {
+    shot = prompt('Ти готовий стріляти? (Веди цифру від 0 до 7');
+    if (shot < 0 || shot > 7) {
+        alert('Ееее, братішка, потрібно від 0 до 7');
+    } else {
+        shots = shots + 1;
+        if (shot == location1 || shot == location2 || shot == location3) {
+            alert("БА-БАХ!!! Ти попав)");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("Чотінько! Ти його завалив)");
+            }
+        } else {
+            alert('Пічалька! Ти промахнувся(');
+        }
+    }
+
+}
