17d62c13932ba18eca437380be860c5d1927cbc9
Dmytro733 <ddp0496@gmail.com>
2020-05-30 00:36:53 +0300

    added automatic ship location

--- Battleship #1-basic java script/battleship-basic java script.js
+++ Battleship #1-basic java script/battleship-basic java script.js
@@ -1,6 +1,8 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let location1 = Math.floor(Math.random() * 5); // added automatic ship location
+let location2 = location1 + 1;
+let location3 = location2 + 1;
+
+alert(location1);
 
 let shot;
 let hits = 0;
@@ -9,21 +11,20 @@ let shots = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-    shot = prompt('Ти готовий стріляти? (Веди цифру від 0 до 7');
-    if (shot < 0 || shot > 7) {
-        alert('Ееее, братішка, потрібно від 0 до 7');
+  shot = prompt("Ти готовий стріляти? (Веди цифру від 0 до 7");
+  if (shot < 0 || shot > 7) {
+    alert("Ееее, братішка, потрібно від 0 до 7");
+  } else {
+    shots = shots + 1;
+    if (shot == location1 || shot == location2 || shot == location3) {
+      alert("БА-БАХ!!! Ти попав)");
+      hits = hits + 1;
+      if (hits == 3) {
+        isSunk = true;
+        alert("Чотінько! Ти його завалив)");
+      }
     } else {
-        shots = shots + 1;
-        if (shot == location1 || shot == location2 || shot == location3) {
-            alert("БА-БАХ!!! Ти попав)");
-            hits = hits + 1;
-            if (hits == 3) {
-                isSunk = true;
-                alert("Чотінько! Ти його завалив)");
-            }
-        } else {
-            alert('Пічалька! Ти промахнувся(');
-        }
+      alert("Пічалька! Ти промахнувся(");
     }
-
+  }
 }
